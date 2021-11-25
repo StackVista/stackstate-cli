24b14e78a742c923f19d989ff28c8679ec6046bd
Lodewijk Bogaards <lbogaards@stackstate.com>
2021-11-25 19:29:01 +0100

    Make sure mocks are not removed from the generated openapi directory

--- generate.sh
+++ generate.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-rm -rf internal/stackstate_client
+
+# Throw everything away except for the mock files. 
+# You ask WHY?
+# The OpenAPI request structs are non-exported members. This means they can not be
+# intercepted in a mock that is not within the same directory. The long term solution
+# would be to generate the mocks along with the rest of the code, but that may be
+# something for later
+find internal/stackstate_client -type f ! -name 'mock_*' -delete
+
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i /local/stackstate_openapi/spec/api.yaml  \
     -g go \
     -c /local/openapi_generator_config.yaml \
     -o /local/internal/stackstate_client
+
+# we need to throw these files away, otherwise go gets upset
 rm internal/stackstate_client/go.mod
 rm internal/stackstate_client/go.sum
