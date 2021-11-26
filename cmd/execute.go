0326332209a3d252869c77d3513ee97efe01695f
Lodewijk Bogaards <lbogaards@stackstate.com>
2021-11-26 16:10:54 +0100

    Seperate execute from root command

---
+++ cmd/execute.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"context"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+	"gitlab.com/stackvista/stackstate-cli2/internal/conf"
+	"gitlab.com/stackvista/stackstate-cli2/internal/di"
+	pr "gitlab.com/stackvista/stackstate-cli2/internal/printer"
+)
+
+const (
+	ConfigErrorExitCode      = 2
+	CommandExecutionExitCode = 1
+)
+
+func Execute(ctx context.Context) {
+	printer := pr.NewStdPrinter()
+
+	cli := &di.Deps{
+		Printer: printer,
+	}
+
+	cmd := AllCommands(cli)
+
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+	cmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		// needs to happen before run, but after execute
+		// so flag-config bindings can take hold
+		cfg, err := conf.ReadConf(cmd)
+		if err != nil {
+			printer.PrintErr(err)
+			os.Exit(ConfigErrorExitCode)
+		}
+		cli.Config = &cfg
+
+		verbose, _ := cmd.Flags().GetCount("verbose")
+		if verbose > 0 {
+			zerolog.SetGlobalLevel(zerolog.TraceLevel)
+		}
+		return nil
+	}
+
+	if err := cmd.ExecuteContext(ctx); err != nil {
+		cli.Printer.PrintErr(err)
+		os.Exit(CommandExecutionExitCode)
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,8 @@
 package cmd
 
 import (
-	"context"
-	"os"
-
-	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
-	"gitlab.com/stackvista/stackstate-cli2/internal/conf"
 	"gitlab.com/stackvista/stackstate-cli2/internal/di"
-	pr "gitlab.com/stackvista/stackstate-cli2/internal/printer"
-)
-
-const (
-	EnvPrefix = "STS"
 )
 
 func RootCommand() *cobra.Command {
@@ -34,36 +24,4 @@ func AllCommands(cli *di.Deps) *cobra.Command {
 	cmd.PersistentFlags().String("api-token", "", "StackState API Token.")
 
 	return cmd
-}
-
-func Execute(ctx context.Context) {
-	printer := pr.NewStdPrinter()
-
-	cli := &di.Deps{
-		Printer: printer,
-	}
-
-	cmd := AllCommands(cli)
-
-	// set verbosity using zerolog
-	zerolog.SetGlobalLevel(zerolog.Disabled)
-	cmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-		cfg, err := conf.ReadConf(cmd)
-		if err != nil {
-			printer.PrintErr(err)
-			os.Exit(1)
-		}
-		cli.Config = &cfg
-
-		verbose, _ := cmd.Flags().GetCount("verbose")
-		if verbose > 0 {
-			zerolog.SetGlobalLevel(zerolog.TraceLevel)
-		}
-		return nil
-	}
-
-	if err := cmd.ExecuteContext(ctx); err != nil {
-		cli.Printer.PrintErr(err)
-		os.Exit(2)
-	}
 }
