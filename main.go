780c6ae7dac8822c46b572affd371688f3bcc4a9
Jeroen van Erp <jeroen@hierynomus.com>
2021-12-30 14:57:13 +0100

    STAC-0: Fix build of CLI2 project

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
-//go:generate pkger
 package main
 
 import (
 	"context"
 
-	"github.com/markbates/pkger"
 	"github.com/rs/zerolog/log"
 	"gitlab.com/stackvista/stackstate-cli2/cmd"
 )
 
 func main() {
-	// Instruct pkger to include the entire /static directory
-	_ = pkger.Include("/static")
 	ctx := log.Logger.WithContext(context.Background())
 	cmd.Execute(ctx)
 }
