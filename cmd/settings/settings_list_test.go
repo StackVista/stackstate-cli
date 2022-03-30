a03879c69ea41ca91904a04b52e439c898ebd75e
shuza <shuza.sa@gmail.com>
2022-03-30 18:35:02 +0200

    add unit test for list namespace and owner

--- cmd/settings/settings_list_test.go
+++ cmd/settings/settings_list_test.go
@@ -60,3 +60,44 @@ func TestSettingsListPrintsToTable(t *testing.T) {
 
 	assert.Equal(t, expectedTableCall, *cli.MockPrinter.TableCalls)
 }
+
+func TestSettingsListWithNamespaeAndOwnerPrintsToTable(t *testing.T) {
+	name := "One"
+	description := "First component"
+	owner := "owner-1"
+	identifier := "urn:stackpack:stackstate-self-health:shared"
+	nodeApiResult := []sts.Node{
+		{
+			Id:                  1,
+			TypeName:            "ComponentType",
+			Identifier:          &identifier,
+			Name:                &name,
+			Description:         &description,
+			OwnedBy:             &owner,
+			LastUpdateTimestamp: 1438167001716,
+		},
+	}
+	expectedUpdateTime := time.UnixMilli(1438167001716).Format("Mon Jan _2 15:04:05 2006")
+	buf := new(bytes.Buffer)
+	if err := json.NewEncoder(buf).Encode([]map[string]interface{}{{"name": "ms_iis_ws"}}); err != nil {
+		t.Fatal(err)
+	}
+
+	cli, cmd := setupCommandFn()
+	cli.MockClient.ApiMocks.NodeApi.TypeListResponse.Response = &http.Response{Body: ioutil.NopCloser(buf)}
+	cli.MockClient.ApiMocks.NodeApi.TypeListResponse.Result = nodeApiResult
+
+	util.ExecuteCommandWithContext(cli.Context, cmd, "--type", "ComponentType",
+		"-n", "component",
+		"-w", "urn:stackpack:stackstate-self-health:shared")
+
+	expectedTableCall := []printer.TableCall{
+		{
+			Header:     []string{"Type", "Id", "Identifier", "Name", "description", "owned by", "last updated"},
+			Data:       [][]string{{"ComponentType", "1", identifier, name, description, owner, expectedUpdateTime}},
+			StructData: []map[string]interface{}{{"name": "ms_iis_ws"}},
+		},
+	}
+
+	assert.Equal(t, expectedTableCall, *cli.MockPrinter.TableCalls)
+}
