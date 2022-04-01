b552b9facdc1d153d8e0fea9818cc631a9a1adc7
Mihails Butorins <mihails.butorins@gmail.com>
2022-04-01 10:14:55 +0200

    Use named package for sts in Nix flake

--- flake.nix
+++ flake.nix
@@ -19,20 +19,24 @@
           ]);
         };
 
-        defaultPackage = pkgs.buildGoModule {
-          pname = "sts";
-          version = "0.0.1";
+        packages = {
+          sts = pkgs.buildGoModule {
+            pname = "sts";
+            version = "0.0.1";
 
-          src = ./.;
+            src = ./.;
 
-          # This hash locks the dependencies of this package.
-          # Change it to the provided when the go dependencies change.
-          # See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for details
-          vendorSha256 = "sha256-vf0zhp2Y8MUJRvyBhJDLpmoooM8I8cboTFxR3JKs2Ns=";
+            # This hash locks the dependencies of this package.
+            # Change it to the provided when the go dependencies change.
+            # See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for details
+            vendorSha256 = "sha256-vf0zhp2Y8MUJRvyBhJDLpmoooM8I8cboTFxR3JKs2Ns=";
 
-          postInstall = ''
+            postInstall = ''
             mv $out/bin/stackstate-cli2 $out/bin/sts
           '';
+          };
         };
+
+        defaultPackage = self.packages.sts;
       });
 }
