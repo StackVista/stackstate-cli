45500e97e1a4e78328f2e969f5af6bf1ed5fe7a8
Bram Schuur <bschuur@stackstate.com>
2022-05-04 09:56:32 +0200

    STAC-16415: Pull openapi spec from the openapi spec repository

--- scripts/generate_stackstate_api.sh
+++ scripts/generate_stackstate_api.sh
@@ -4,8 +4,15 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
+OPENAPI_VERSION=`cat openapi_version`
+CHECKOUT_DIR="stackstate_openapi/checkout"
+
+rm -rf "$CHECKOUT_DIR"
+git clone https://gitlab.com/stackvista/platform/stackstate-openapi.git "$CHECKOUT_DIR"
+git -C "$CHECKOUT_DIR" checkout "$OPENAPI_VERSION"
+
 rm -rf generated/stackstate_api
-nix develop -c openapi-generator-cli generate -i stackstate_openapi/spec/api.yaml -g go  -c stackstate_openapi/openapi_generator_config.yaml -o generated/stackstate_api -t stackstate_openapi/template
+nix develop -c openapi-generator-cli generate -i "$CHECKOUT_DIR/spec/api.yaml" -g go  -c stackstate_openapi/openapi_generator_config.yaml -o generated/stackstate_api -t stackstate_openapi/template
 
 # we need to throw these files away, otherwise go gets upset
 rm generated/stackstate_api/go.mod
