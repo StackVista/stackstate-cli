50835bc4f28d6a60c798805d1ea6779957b59e15
Lodewijk Bogaards <lbogaards@stackstate.com>
2022-05-05 20:49:09 +0200

    Update latest version script

--- scripts/print_latest_version.sh
+++ scripts/print_latest_version.sh
@@ -2,7 +2,7 @@
 set -e && cd "$(dirname "$0")" && cd ..
 
 # Make sure we've got all tags
-git remote update 2>&1 > /dev/null
+git remote update > /dev/null 2>&1
 
 # Prints the latest version of the StackState CLI based on git semver git tags starting with a "v" and a number
 TAG_NAME=`git tag --list "v[0-9]*" --sort v:refname | tail -1`
