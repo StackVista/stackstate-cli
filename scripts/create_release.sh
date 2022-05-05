a156007d4dcdf5999d1bbf6e0f7a81765516baec
Lodewijk Bogaards <lbogaards@stackstate.com>
2022-05-05 13:30:25 +0200

    Add create release script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e && cd "$(dirname "$0")" && cd ..
 
+echo "Updating git from remote"
+git remote update
 
-
-if output=$(git status --porcelain) && [ -z "$output" ]; then
+if [ -n "$(git status --porcelain)" ]; then
   echo "You've got uncommitted changes in tracked files. Please make sure you're on a clean commit.";
   exit 1
 fi
