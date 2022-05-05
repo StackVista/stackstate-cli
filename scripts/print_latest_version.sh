d9b358cc593f31bec9c4b57df6e78fc68bc27232
Lodewijk Bogaards <lbogaards@stackstate.com>
2022-05-05 14:54:33 +0200

    Cut starting 'v' out of LATEST_VERSION file

--- scripts/print_latest_version.sh
+++ scripts/print_latest_version.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-set -e
-cd "$(dirname "$0")" && cd ..
+set -e && cd "$(dirname "$0")" && cd ..
+
+# Make sure we've got all tags
+git remote update 2>&1 > /dev/null
 
 # Prints the latest version of the StackState CLI based on git semver git tags starting with a "v" and a number
-git tag --list "v[0-9]*" --sort v:refname | tail -1
+TAG_NAME=`git tag --list "v[0-9]*" --sort v:refname | tail -1`
+VERSION=${TAG_NAME:1}
+echo $VERSION
