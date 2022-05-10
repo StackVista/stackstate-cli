cf94f3b69c0b145ca3d61b497fb1d8101515bf58
Frank van Lankvelt <fvanlankvelt@stackstate.com>
2022-05-10 08:54:12 +0200

    STAC-16385: accept absolute time, verify request parameters, add example

--- cmd/anomaly/anomaly_collect.go
+++ cmd/anomaly/anomaly_collect.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -24,18 +25,58 @@ const (
 func AnomalyCollect(cli *di.Deps) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "collect",
-		Short: "collect anomaly feedback",
-		Long:  "Fetch anomaly annotations with feedback (thumbs-up/down and/or comments)",
-		RunE:  cli.CmdRunEWithApi(RunCollectFeedbackCommand),
+		Short: "collect anomalies that have user feedback",
+		Long:  "Fetch anomalies that users have given feedback on, as thumbs-up/-down and/or comments",
+		Example: "# Export anomalies with feedback in the last 7 days, include 1 day of metric data for each anomaly" +
+			`sts anomaly collect --start-time=-168h --file anomaly-feedback.json"`,
+		RunE: cli.CmdRunEWithApi(RunCollectFeedbackCommand),
 	}
-	cmd.Flags().DurationP(StartTimeFlag, "", 0, "start time of interval with anomalies")
+	cmd.Flags().StringP(StartTimeFlag, "", "-7d", "start time of interval with anomalies.  Format is ISO8601, milliseconds since epoch or relative (-12h)")
 	cmd.MarkFlagRequired(StartTimeFlag) //nolint:errcheck
 	cmd.Flags().StringP(FileFlag, "f", "", "path to output file")
 	cmd.MarkFlagRequired(FileFlag) //nolint:errcheck
-	cmd.Flags().DurationP(EndTimeFlag, "", 0, "end time of interval with anomalies")
+	cmd.Flags().StringP(EndTimeFlag, "", "-0h", "end time of interval with anomalies")
 	cmd.Flags().DurationP(HistoryFlag, "d", Day, "length of metric data history preceding the anomaly")
 
 	return cmd
+}
+
+type Clock interface {
+	Now() time.Time
+}
+
+type StdClock struct{}
+
+func newClock() Clock {
+	return &StdClock{}
+}
+
+func (clock *StdClock) Now() time.Time {
+	return time.Now()
+}
+
+func NewTimeParseError(value string) common.CLIError {
+	return common.StdCLIError{
+		Err: fmt.Errorf("invalid time format %s - expected ISO8601 (2016-09-13T13:12:04Z), milliseconds since epoch or relative time (-168h) ", value),
+	}
+}
+
+func parseTime(clock Clock, strStartTime string) (time.Time, common.CLIError) {
+	var startTime time.Time
+	startTimeInt, err := strconv.ParseInt(strStartTime, 0, 64) //nolint:gomnd
+	if err != nil {
+		startTime, err = time.Parse(time.RFC3339, strStartTime)
+		if err != nil {
+			duration, err := time.ParseDuration(strStartTime)
+			if err != nil {
+				return time.Time{}, NewTimeParseError(strStartTime)
+			}
+			startTime = clock.Now().Add(duration)
+		}
+	} else {
+		startTime = time.UnixMilli(startTimeInt)
+	}
+	return startTime, nil
 }
 
 func RunCollectFeedbackCommand(
@@ -44,9 +85,15 @@ func RunCollectFeedbackCommand(
 	api *stackstate_api.APIClient,
 	serverInfo *stackstate_api.ServerInfo,
 ) common.CLIError {
-	startTime, err := cmd.Flags().GetDuration(StartTimeFlag)
+	clock := newClock()
+
+	strStartTime, err := cmd.Flags().GetString(StartTimeFlag)
 	if err != nil {
 		return common.NewCLIArgParseError(err)
+	}
+	startTime, parseErr := parseTime(clock, strStartTime)
+	if parseErr != nil {
+		return parseErr
 	}
 
 	filePath, err := cmd.Flags().GetString(FileFlag)
@@ -54,9 +101,13 @@ func RunCollectFeedbackCommand(
 		return common.NewCLIArgParseError(err)
 	}
 
-	endTime, err := cmd.Flags().GetDuration(EndTimeFlag)
+	strEndTime, err := cmd.Flags().GetString(EndTimeFlag)
 	if err != nil {
 		return common.NewCLIArgParseError(err)
+	}
+	endTime, parseErr := parseTime(clock, strEndTime)
+	if parseErr != nil {
+		return parseErr
 	}
 
 	history, err := cmd.Flags().GetDuration(HistoryFlag)
@@ -64,10 +115,9 @@ func RunCollectFeedbackCommand(
 		return common.NewCLIArgParseError(err)
 	}
 
-	now := time.Now().UnixMilli()
 	feedback, resp, err := api.AnomalyFeedbackApi.CollectAnomalyFeedback(cli.Context).
-		StartTime(now + startTime.Milliseconds()).
-		EndTime(now + endTime.Milliseconds()).
+		StartTime(startTime.UnixMilli()).
+		EndTime(endTime.UnixMilli()).
 		History(history.Milliseconds()).
 		Execute()
 	if err != nil {
--- cmd/anomaly/anomaly_collect_test.go
+++ cmd/anomaly/anomaly_collect_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
@@ -17,6 +18,38 @@ func setupCommandCollect() (di.MockDeps, *cobra.Command) {
 	cmd := AnomalyCollect(&mockCli.Deps)
 
 	return mockCli, cmd
+}
+
+type TestClock struct {
+	now int64
+}
+
+func newTestClock() Clock {
+	return &TestClock{
+		now: 1652108645000,
+	}
+}
+
+func (clock *TestClock) Now() time.Time {
+	return time.UnixMilli(clock.now)
+}
+
+func TestTimeParsing(t *testing.T) {
+	clock := newTestClock()
+	parsedTime, err := parseTime(clock, "-2h")
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1652108645000-7200000), parsedTime.UnixMilli())
+
+	parsedTime, err = parseTime(clock, "2022-05-09T15:04:05Z")
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1652108645000), parsedTime.UnixMilli())
+
+	parsedTime, err = parseTime(clock, "1652108645000")
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1652108645000), parsedTime.UnixMilli())
+
+	_, err = parseTime(clock, "-2d")
+	assert.NotNil(t, err)
 }
 
 func TestAnomalyCollectEmpty(t *testing.T) {
@@ -37,9 +70,15 @@ func TestAnomalyCollectEmpty(t *testing.T) {
 	cli, cmd := setupCommandCollect()
 	cli.MockClient.ApiMocks.AnomalyFeedbackApi.CollectAnomalyFeedbackResponse.Result = feedback
 
-	_, err = di.ExecuteCommandWithContext(&cli.Deps, cmd, "--start-time=-1h", "--file", filePath)
+	_, err = di.ExecuteCommandWithContext(&cli.Deps, cmd, "--start-time", "1652108645000", "--end-time", "1652108845000", "--file", filePath)
 	assert.Nil(t, err)
-	assert.Equal(t, []string{}, *cli.MockPrinter.PrintLnCalls)
+	assert.Equal(t, []string{"Downloaded 2 anomalies with feedback."}, *cli.MockPrinter.SuccessCalls)
+
+	calls := *cli.MockClient.ApiMocks.AnomalyFeedbackApi.CollectAnomalyFeedbackCalls
+	assert.Equal(t, 1, len(calls))
+	assert.Equal(t, int64(1652108645000), *calls[0].PstartTime)
+	assert.Equal(t, int64(1652108845000), *calls[0].PendTime)
+	assert.Equal(t, int64(86400000), *calls[0].Phistory)
 
 	body, err := ioutil.ReadFile(filePath)
 	assert.Nil(t, err)
