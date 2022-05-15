44dadedaadbe7d85dcc591f9ce9891b0c9643b42
Lodewijk Bogaards <lbogaards@stackstate.com>
2022-05-15 22:32:24 +0200

    STAC-16567 Handle primitive pointers better in util.ToString

--- internal/util/string_util.go
+++ internal/util/string_util.go
@@ -57,22 +57,49 @@ func WithNewLine(s string) string {
 }
 
 func ToString(x interface{}) string {
-	if x == nil {
-		return "nil"
+	r := reflect.ValueOf(x)
+	k := r.Kind()
+
+	isPointer := k == reflect.Chan || k == reflect.Func || k == reflect.Map || k == reflect.Pointer ||
+		k == reflect.UnsafePointer || k == reflect.Slice || k == reflect.Interface
+
+	if k == reflect.Invalid || (isPointer && r.IsNil()) {
+		return "-"
 	}
-	if reflect.ValueOf(x).Type() == reflect.TypeOf(time.Time{}) {
+	if r.Type() == reflect.TypeOf(time.Time{}) {
 		return TimeToString(x)
 	}
 
-	switch v := x.(type) {
-	case *string:
-		return SafeStringPtrToString(v)
+	// by this time we know that all null pointer are already handled
+	// so we can simply dereference any pointer we have
+	switch z := x.(type) {
+	case *int8:
+		x = *z
+	case *uint8:
+		x = *z
+	case *int16:
+		x = *z
+	case *uint16:
+		x = *z
+	case *int32:
+		x = *z
+	case *uint32:
+		x = *z
 	case *int64:
-		return SafeIntPointerToString(v)
+		x = *z
+	case *uint64:
+		x = *z
+	case *float32:
+		x = *z
+	case *float64:
+		x = *z
+	}
+
+	switch v := x.(type) {
 	case float64:
 		i, err := safeConvertFloat64ToInt64(v)
 		if err != nil {
-			return fmt.Sprintf("%f", v)
+			return fmt.Sprintf("%v", v)
 		} else {
 			return fmt.Sprintf("%d", i)
 		}
@@ -90,14 +117,6 @@ func safeConvertFloat64ToInt64(f float64) (int64, error) {
 		return 0, fmt.Errorf("lost precision during conversion")
 	} else {
 		return v, nil
-	}
-}
-
-func SafeStringPtrToString(s *string) string {
-	if s == nil {
-		return "-"
-	} else {
-		return *s
 	}
 }
 
--- internal/util/string_util_test.go
+++ internal/util/string_util_test.go
@@ -3,6 +3,7 @@ package util
 import (
 	"math"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,21 +14,68 @@ func TestUcFirst(t *testing.T) {
 	assert.Equal(t, UcFirst("ést"), "Ést")
 }
 
-func TestToStringPrintNaN(t *testing.T) {
-	assert.Equal(t, ToString(math.NaN()), "NaN")
-}
-func TestToStringPrintInf(t *testing.T) {
-	assert.Equal(t, ToString(math.Inf(1)), "+Inf")
-}
-
-func TestToStringPrintLargeNum(t *testing.T) {
-	assert.Equal(t, ToString(107915343054010.0), "107915343054010")
-}
-
-func TestToStringNil(t *testing.T) {
-	assert.Equal(t, ToString(nil), "nil")
-}
-
 func TestUniqueString(t *testing.T) {
 	assert.Equal(t, []string{"foo", "bar"}, UniqueStrings([]string{"foo", "bar", "foo", "bar"}))
+}
+
+func TestToString(t *testing.T) {
+	var f32nil *float32
+	var mapnil map[string]string
+
+	i8 := byte(7)
+	ui8 := byte(7)
+	i16 := int16(-15)
+	ui16 := uint16(15)
+	i32 := int32(-42)
+	ui32 := uint32(42)
+	i64 := int64(-42)
+	ui64 := uint64(42)
+	f32 := float32(3.14)
+	f64 := float32(3.14)
+
+	tests := map[string]struct {
+		input    interface{}
+		expected string
+	}{
+		"int8":          {input: int8(-7), expected: "-7"},
+		"pint8":         {input: &i8, expected: "7"},
+		"ui8":           {input: uint8(7), expected: "7"},
+		"pui8":          {input: &ui8, expected: "7"},
+		"int16":         {input: int16(-15), expected: "-15"},
+		"pint16":        {input: &i16, expected: "-15"},
+		"uint16":        {input: uint16(15), expected: "15"},
+		"puint16":       {input: &ui16, expected: "15"},
+		"int32":         {input: int32(-42), expected: "-42"},
+		"pint32":        {input: &i32, expected: "-42"},
+		"uint32":        {input: uint32(42), expected: "42"},
+		"puint32":       {input: &ui32, expected: "42"},
+		"int64":         {input: int64(-42), expected: "-42"},
+		"pint64":        {input: &i64, expected: "-42"},
+		"uint64":        {input: uint64(42), expected: "42"},
+		"puint64":       {input: &ui64, expected: "42"},
+		"float32":       {input: float32(3.14), expected: "3.14"},
+		"pfloat32":      {input: &f32, expected: "3.14"},
+		"float32 round": {input: float32(3), expected: "3"},
+		"float64":       {input: float64(3.14), expected: "3.14"},
+		"pfloat64":      {input: &f64, expected: "3.14"},
+		"float64 round": {input: float64(3), expected: "3"},
+		"float64 round large no scientific notiation": {
+			input: float64(107915343054010.0), expected: "107915343054010",
+		},
+		"positive infinity": {input: math.Inf(1), expected: "+Inf"},
+		"negative infinity": {input: math.Inf(-1), expected: "-Inf"},
+		"not a number":      {input: math.NaN(), expected: "NaN"},
+		"nil":               {input: nil, expected: "-"},
+		"float32 nil":       {input: f32nil, expected: "-"},
+		"time":              {input: time.UnixMilli(0), expected: "Thu Jan 1 01:00:00 1970 CET"},
+		"map nil":           {input: mapnil, expected: "-"},
+		"string":            {input: "hello world", expected: "hello world"},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			output := ToString(tc.input)
+			assert.Equal(t, tc.expected, output)
+		})
+	}
 }
