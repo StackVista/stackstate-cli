5622f46c91865c98c965b2928a7ff3da333d578a
Jeroen van Erp <jeroen@hierynomus.com>
2022-05-16 11:06:58 +0200

    STAC-0: Rename 'cli' to 'cli-config'

--- cmd/cliconfig/cliconfig_validate.go
+++ cmd/cliconfig/cliconfig_validate.go
@@ -1,4 +1,4 @@
-package cli
+package cliconfig
 
 import (
 	"github.com/spf13/cobra"
@@ -7,15 +7,15 @@ import (
 	"gitlab.com/stackvista/stackstate-cli2/internal/di"
 )
 
-func CliTestCommandCommand(cli *di.Deps) *cobra.Command {
+func ValidateCommand(cli *di.Deps) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "test-connect",
-		Short: "test connection to the configured StackState",
-		Long:  "Test connection to the configured StackState or to a different StackState by passing flags.",
-		Example: "# test connection to the configured StackState\n" +
-			"sts cli test-connect\n" +
-			"# test connection to my.stackstate.com \n" +
-			"sts cli test-connect --api-token l9x5g14cMcI97IS4785HWgwEpdPr3KJ4 --api-url \"https://my.stackstate.com/api\"",
+		Use:   "validate",
+		Short: "validate connection to the configured StackState",
+		Long:  "Validate connection to the configured StackState or to a different StackState by passing flags.",
+		Example: "# validate connection to the configured StackState\n" +
+			"sts cli-config validate\n" +
+			"# validate connection to my.stackstate.com \n" +
+			"sts cli-config validate --api-token l9x5g14cMcI97IS4785HWgwEpdPr3KJ4 --api-url \"https://my.stackstate.com/api\"",
 		RunE: cli.CmdRunEWithApi(RunTestConnectConfig),
 	}
 	return cmd
