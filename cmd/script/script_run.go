89bb74b68c4d272a956c1bd78ea88db37d1972e1
Jeroen van Erp <jeroen@hierynomus.com>
2022-05-17 21:21:45 +0200

    Migrated script run to new flag parsing
    Signed-off-by: Jeroen van Erp <jeroen@hierynomus.com>

--- cmd/script/script_run.go
+++ cmd/script/script_run.go
@@ -8,7 +8,15 @@ import (
 	"gitlab.com/stackvista/stackstate-cli2/internal/common"
 	"gitlab.com/stackvista/stackstate-cli2/internal/di"
 	"gitlab.com/stackvista/stackstate-cli2/internal/mutex_flags"
+	"gitlab.com/stackvista/stackstate-cli2/internal/util"
 )
+
+type ScriptRunArgs struct {
+	Script          string
+	ArgumentsScript string
+	TimeoutMs       int32
+	ScriptFile      string
+}
 
 const (
 	ScriptFlag          = "script"
@@ -17,6 +25,7 @@ const (
 )
 
 func ScriptRunCommand(cli *di.Deps) *cobra.Command {
+	args := &ScriptRunArgs{}
 	cmd := &cobra.Command{
 		Use:   "run",
 		Short: "run a STSL script",
@@ -26,90 +35,71 @@ func ScriptRunCommand(cli *di.Deps) *cobra.Command {
 			"\n" +
 			"# run a script with variables provided by an arguments-script\n" +
 			"sts script run --script \"x+y\" --arguments-script \"[x: 1, y: 2]\"",
-		RunE: cli.CmdRunEWithApi(RunScriptRunCommand),
+		RunE: cli.CmdRunEWithApi(RunScriptRunCommand(args)),
 	}
 
-	cmd.Flags().String(ScriptFlag, "", "a script to run")
-	cmd.Flags().String(ArgumentsScriptFlag, "",
+	cmd.Flags().StringVar(&args.Script, ScriptFlag, "", "a script to run")
+	cmd.Flags().StringVar(&args.ArgumentsScript, ArgumentsScriptFlag, "",
 		"an extra script that generates arguments to be used as variables when the main script is executed, return format: java.util.Map",
 	)
-	cmd.Flags().IntP(TimeoutFlag, "t", 0, "timeout in milli-seconds for script execution")
-	common.AddFileFlag(cmd, "path to a file containing the script to run")
+	cmd.Flags().Int32VarP(&args.TimeoutMs, TimeoutFlag, "t", 0, "timeout in milli-seconds for script execution")
+	common.AddFileFlagVar(cmd, &args.ScriptFile, "path to a file containing the script to run")
 	mutex_flags.MarkMutexFlags(cmd, []string{ScriptFlag, common.FileFlag}, "input", true)
 
 	return cmd
 }
 
-func RunScriptRunCommand(
-	cmd *cobra.Command,
-	cli *di.Deps,
-	api *stackstate_api.APIClient,
-	serverInfo *stackstate_api.ServerInfo,
-) common.CLIError {
-	var script string
+func RunScriptRunCommand(args *ScriptRunArgs) di.CmdWithApiFn {
+	return func(
+		cmd *cobra.Command,
+		cli *di.Deps,
+		api *stackstate_api.APIClient,
+		serverInfo *stackstate_api.ServerInfo,
+	) common.CLIError {
+		script := args.Script
 
-	script, err := cmd.Flags().GetString(ScriptFlag)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-	filepath, err := cmd.Flags().GetString(common.FileFlag)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-
-	err = mutex_flags.CheckMutuallyExclusiveFlags(cmd, []string{ScriptFlag, common.FileFlag}, true)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-
-	if filepath != "" {
-		b, err := os.ReadFile(filepath)
+		err := mutex_flags.CheckMutuallyExclusiveFlags(cmd, []string{ScriptFlag, common.FileFlag}, true)
 		if err != nil {
-			return common.NewReadFileError(err, filepath)
+			return common.NewCLIArgParseError(err)
 		}
-		script = string(b)
-	}
 
-	var argumentsScript *string
-	a, _ := cmd.Flags().GetString(ArgumentsScriptFlag)
-	if a != "" {
-		argumentsScript = &a
-	}
-	var timeoutMs *int32
-	t, _ := cmd.Flags().GetInt(TimeoutFlag)
-	if t != 0 {
-		t32 := int32(t)
-		timeoutMs = &t32
-	}
+		if args.ScriptFile != "" {
+			b, err := os.ReadFile(args.ScriptFile)
+			if err != nil {
+				return common.NewReadFileError(err, args.ScriptFile)
+			}
+			script = string(b)
+		}
 
-	// execute script
-	scriptRequest := stackstate_api.ExecuteScriptRequest{
-		TimeoutMs:       timeoutMs,
-		Script:          script,
-		ArgumentsScript: argumentsScript,
-	}
+		// execute script
+		scriptRequest := stackstate_api.ExecuteScriptRequest{
+			TimeoutMs:       util.Int32NilP(args.TimeoutMs),
+			Script:          script,
+			ArgumentsScript: util.StringNilP(args.ArgumentsScript),
+		}
 
-	scriptResponse, resp, err := api.ScriptingApi.
-		ScriptExecute(cli.Context).
-		ExecuteScriptRequest(scriptRequest).
-		Execute()
-	if err != nil {
-		return common.NewResponseError(err, resp)
-	}
+		scriptResponse, resp, err := api.ScriptingApi.
+			ScriptExecute(cli.Context).
+			ExecuteScriptRequest(scriptRequest).
+			Execute()
+		if err != nil {
+			return common.NewResponseError(err, resp)
+		}
 
-	// print response
-	value := scriptResponse.Result["value"]
-	if cli.IsJson {
-		cli.Printer.PrintJson(map[string]interface{}{
-			"result": scriptResponse.Result,
-		})
-	} else {
-		if value == nil {
-			cli.Printer.Success("script executed (no response)")
+		// print response
+		value := scriptResponse.Result["value"]
+		if cli.IsJson {
+			cli.Printer.PrintJson(map[string]interface{}{
+				"result": scriptResponse.Result,
+			})
 		} else {
-			cli.Printer.PrintStruct(value)
+			if value == nil {
+				cli.Printer.Success("script executed (no response)")
+			} else {
+				cli.Printer.PrintStruct(value)
+			}
 		}
-	}
 
-	return nil
+		return nil
+	}
 }
