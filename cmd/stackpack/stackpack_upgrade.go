70a21ebd3f628996f365cd72e0f3bb5595589603
shuza <shuza.sa@gmail.com>
2022-05-18 21:31:06 +0200

    update flag parse according to new flow

--- cmd/stackpack/stackpack_upgrade.go
+++ cmd/stackpack/stackpack_upgrade.go
@@ -16,62 +16,61 @@ const (
 	strategySkip  = "skip"
 )
 
+type UpgradeArgs struct {
+	TypeName         string
+	UnlockedStrategy string
+}
+
 func StackpackUpgradeCommand(cli *di.Deps) *cobra.Command {
+	args := &UpgradeArgs{}
 	cmd := &cobra.Command{
 		Use:   "upgrade",
 		Short: "upgrade a StackPack",
 		Long:  "Upgrade a StackPack instance to the next version.",
-		RunE:  cli.CmdRunEWithApi(RunStackpackUpgradeCommand),
+		RunE:  cli.CmdRunEWithApi(RunStackpackUpgradeCommand(args)),
 	}
-	common.AddRequiredNameFlag(cmd, "name of the StackPack")
-	cmd.Flags().String(UnlockedStrategyFlag, "", "unlocked-strategy flag must be one of {fail,overwrite,skip}")
+	common.AddRequiredNameFlagVar(cmd, &args.TypeName, "name of the StackPack")
+	cmd.Flags().StringVar(&args.UnlockedStrategy, UnlockedStrategyFlag, "", "unlocked-strategy flag must be one of {fail,overwrite,skip}")
 	cmd.MarkFlagRequired(UnlockedStrategyFlag) //nolint:errcheck
 	return cmd
 }
+func RunStackpackUpgradeCommand(args *UpgradeArgs) di.CmdWithApiFn {
+	return func(
+		cmd *cobra.Command,
+		cli *di.Deps,
+		api *stackstate_api.APIClient,
+		serverInfo *stackstate_api.ServerInfo,
+	) common.CLIError {
+		if !isValidStrategy(args.UnlockedStrategy) {
+			return common.NewCLIArgParseError(errors.New("invalid strategy"))
+		}
+		stackpackList, resp, err := api.StackpackApi.StackpackList(cli.Context).Execute()
+		if err != nil {
+			return common.NewResponseError(err, resp)
+		}
+		stack, err := findStackName(stackpackList, args.TypeName)
+		if err != nil {
+			return common.NewCLIArgParseError(err)
+		}
+		if !stack.HasNextVersion() {
+			return common.NewCLIArgParseError(fmt.Errorf("stackpack %s cannot be upgraded at this moment", args.TypeName))
+		}
+		_, resp, err = api.StackpackApi.UpgradeStackPack(cli.Context, args.TypeName).Unlocked(args.UnlockedStrategy).Execute()
+		if err != nil {
+			return common.NewResponseError(err, resp)
+		}
+		if cli.IsJson {
+			cli.Printer.PrintJson(map[string]interface{}{
+				"success":         true,
+				"current-version": stack.GetVersion(),
+				"next-version":    stack.NextVersion.GetVersion(),
+			})
+		} else {
+			cli.Printer.Success(fmt.Sprintf("Successfully triggered upgrade from %s to %s", stack.GetVersion(), stack.NextVersion.GetVersion()))
+		}
 
-func RunStackpackUpgradeCommand(
-	cmd *cobra.Command,
-	cli *di.Deps,
-	api *stackstate_api.APIClient,
-	serverInfo *stackstate_api.ServerInfo,
-) common.CLIError {
-	name, err := cmd.Flags().GetString(common.NameFlag)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
+		return nil
 	}
-	unlockStrategy, err := cmd.Flags().GetString(UnlockedStrategyFlag)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-	if !isValidStrategy(unlockStrategy) {
-		return common.NewCLIArgParseError(errors.New("invalid strategy"))
-	}
-	stackpackList, resp, err := api.StackpackApi.StackpackList(cli.Context).Execute()
-	if err != nil {
-		return common.NewResponseError(err, resp)
-	}
-	stack, err := findStackName(stackpackList, name)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-	if !stack.HasNextVersion() {
-		return common.NewCLIArgParseError(fmt.Errorf("stackpack %s cannot be upgraded at this moment", name))
-	}
-	_, resp, err = api.StackpackApi.UpgradeStackPack(cli.Context, name).Unlocked(unlockStrategy).Execute()
-	if err != nil {
-		return common.NewResponseError(err, resp)
-	}
-	if cli.IsJson {
-		cli.Printer.PrintJson(map[string]interface{}{
-			"success":         true,
-			"current-version": stack.GetVersion(),
-			"next-version":    stack.NextVersion.GetVersion(),
-		})
-	} else {
-		cli.Printer.Success(fmt.Sprintf("Successfully triggered upgrade from %s to %s", stack.GetVersion(), stack.NextVersion.GetVersion()))
-	}
-
-	return nil
 }
 
 func isValidStrategy(name string) bool {
