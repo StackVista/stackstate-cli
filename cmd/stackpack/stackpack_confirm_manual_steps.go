02cd7287379cdc2d7e9389b36d9c0d32c6ed9092
shuza <shuza.sa@gmail.com>
2022-05-19 23:53:36 +0200

    update open api version

--- cmd/stackpack/stackpack_confirm_manual_steps.go
+++ cmd/stackpack/stackpack_confirm_manual_steps.go
@@ -9,47 +9,46 @@ import (
 	"gitlab.com/stackvista/stackstate-cli2/internal/di"
 )
 
+type ManualStepsArgs struct {
+	TypeName string
+	ID       int64
+}
+
 func StackpackConfirmManualStepsCommand(cli *di.Deps) *cobra.Command {
+	args := &ManualStepsArgs{}
 	cmd := &cobra.Command{
 		Use:   "confirm-manual-steps",
 		Short: "confirm manual steps of StackPack",
 		Long: "During some installation of StackPacks manual actions/step must be taken by the user in order for the installation to complete. " +
 			"These steps can not be verified by the system and only require a confirmation from the user. This command sends such a confirmation.",
-		RunE: cli.CmdRunEWithApi(RunStackpackConfirmManualStepsCommand),
+		RunE: cli.CmdRunEWithApi(RunStackpackConfirmManualStepsCommand(args)),
 	}
-	common.AddRequiredNameFlag(cmd, "name of the StackPack")
-	cmd.Flags().Int64(common.IDFlag, 0, "id of the StackPack instance")
-	cmd.MarkFlagRequired(common.IDFlag) //nolint:errcheck
+	common.AddRequiredNameFlagVar(cmd, &args.TypeName, "name of the StackPack")
+	common.AddRequiredIDFlagVar(cmd, &args.ID, "id of the StackPack instance")
 	return cmd
 }
 
-func RunStackpackConfirmManualStepsCommand(
-	cmd *cobra.Command,
-	cli *di.Deps,
-	api *stackstate_api.APIClient,
-	serverInfo *stackstate_api.ServerInfo,
-) common.CLIError {
-	name, err := cmd.Flags().GetString(common.NameFlag)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-	id, err := cmd.Flags().GetInt64(common.IDFlag)
-	if err != nil {
-		return common.NewCLIArgParseError(err)
-	}
-	_, resp, err := api.StackpackApi.ConfirmManualSteps(cli.Context, name, id).Execute()
-	if err != nil {
-		return common.NewResponseError(err, resp)
-	}
-	if cli.IsJson {
-		cli.Printer.PrintJson(map[string]interface{}{
-			"success":               true,
-			"stackpack-name":        name,
-			"stackpack-instance-id": id,
-		})
-	} else {
-		cli.Printer.Success(fmt.Sprintf("confirmation of manual steps of the provisioning StackPack Name: %s StackPack Id: %d", name, id))
-	}
+func RunStackpackConfirmManualStepsCommand(args *ManualStepsArgs) di.CmdWithApiFn {
+	return func(
+		cmd *cobra.Command,
+		cli *di.Deps,
+		api *stackstate_api.APIClient,
+		serverInfo *stackstate_api.ServerInfo,
+	) common.CLIError {
+		_, resp, err := api.StackpackApi.ConfirmManualSteps(cli.Context, args.TypeName, args.ID).Execute()
+		if err != nil {
+			return common.NewResponseError(err, resp)
+		}
+		if cli.IsJson {
+			cli.Printer.PrintJson(map[string]interface{}{
+				"success":               true,
+				"stackpack-name":        name,
+				"stackpack-instance-id": id,
+			})
+		} else {
+			cli.Printer.Success(fmt.Sprintf("confirmation of manual steps of the provisioning StackPack Name: %s StackPack Id: %d", name, id))
+		}
 
-	return nil
+		return nil
+	}
 }
