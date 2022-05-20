dc2f129ac90c10de475fb03ae2748b8b2dfdfc4e
Lodewijk Bogaards <lbogaards@stackstate.com>
2022-05-21 00:12:37 +0200

    Check for arch in install.sh

--- scripts/publish/installers/install.sh
+++ scripts/publish/installers/install.sh
@@ -1,25 +1,34 @@
-#/bin/bash
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+function error() {
+  echo "${RED}[ERROR]${NO_COLOR} $1"
+  exit 10
+}
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   OS=linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   OS=darwin
 else
-  echo "OS $OSTYPE not supported. Please checkout the CLI docs on doc.stackstate.com or contact StackState support for help with your OS."
-  exit 1
+  error "Unsupported operating system: $OSTYPE. Please checkout the CLI docs on docs.stackstate.com or contact StackState for support with your OS."
+fi
+ARCH=`uname -m`
+if [[ "$ARCH" != "x86_64" && "$ARCH" != "arm64" ]]; then
+  error "Unsupported architecture: $ARCH. Please checkout the CLI docs on docs.stackstate.com or contact StackState for support with your OS."
 fi
 
 VERSION=`curl https://dl.stackstate.com/stackstate-cli/LATEST_VERSION 2> /dev/null` 
-ARCH=`uname -m`
 DL="https://dl.stackstate.com/stackstate-cli/v$VERSION/stackstate-cli-full-$VERSION.$OS-$ARCH.tar.gz"
 echo "Downloading and unpacking: $DL"
 curl $DL | tar xz --directory /usr/local/bin
 
-# configure the CLI if the STS_URL And STS_API_TOKEN are given
+# configure the CLI if the STS_URL and STS_API_TOKEN are provided
 if [[ -n "$STS_URL" && -n "$STS_API_TOKEN" ]]; then
   sts cli-config save --url $STS_URL --api-token $STS_API_TOKEN
 fi
 
-GREEN='\033[0;32m'
-NO_COLOR='\033[0m'
-printf "Success! Type '${GREEN}sts${NO_COLOR}' to get started!\n"
+printf "Success! Type ${GREEN}sts${NO_COLOR} to get started!\n"
