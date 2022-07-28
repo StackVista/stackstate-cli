f91edcdb7c1805c6af9137a707ed551c63fef794
shuza <shuza.sa@gmail.com>
2022-07-28 11:50:04 +0200

    refactor health list command

--- cmd/health/health_list.go
+++ cmd/health/health_list.go
@@ -32,57 +32,67 @@ func RunHealthListCommand(args *ListArgs) di.CmdWithApiFn {
 		api *stackstate_api.APIClient,
 		serverInfo *stackstate_api.ServerInfo,
 	) common.CLIError {
-		data := make([][]interface{}, 0)
 		if args.Urn != "" {
 			subList, resp, err := api.HealthSynchronizationApi.GetHealthSynchronizationSubStreamOverview(cli.Context, args.Urn).Execute()
 			if err != nil {
 				return common.NewResponseError(err, resp)
 			}
-			jsonMap := make([]map[string]interface{}, 0)
-			for _, v := range subList.GetSubStreams() {
-				data = append(data, []interface{}{v.GetSubStreamId(), v.GetCheckStateCount()})
-				jsonMap = append(jsonMap, map[string]interface{}{
-					"sub_stream_id":     v.GetSubStreamId(),
-					"check_state_count": v.GetCheckStateCount(),
-				})
-			}
-			if cli.IsJson() {
-				cli.Printer.PrintJson(map[string]interface{}{
-					"sub-stream": jsonMap,
-				})
-			} else {
-				cli.Printer.Table(printer.TableData{
-					Header:              []string{"Sub stream id", "Check state count"},
-					Data:                data,
-					MissingTableDataMsg: printer.NotFoundMsg{Types: "health sub-stream"},
-				})
-			}
+			printSubStreamList(cli, subList)
 		} else {
 			streamList, resp, err := api.HealthSynchronizationApi.GetHealthSynchronizationStreamsOverview(cli.Context).Execute()
 			if err != nil {
 				return common.NewResponseError(err, resp)
 			}
-			jsonMap := make([]map[string]interface{}, 0)
-			for _, v := range streamList.GetItems() {
-				data = append(data, []interface{}{v.GetUrn(), v.GetConsistencyModel(), v.GetSubStreams()})
-				jsonMap = append(jsonMap, map[string]interface{}{
-					"stream_urn":               v.GetUrn(),
-					"stream_consistency_model": v.GetConsistencyModel(),
-					"sub_stream_count":         v.GetSubStreams(),
-				})
-			}
-			if cli.IsJson() {
-				cli.Printer.PrintJson(map[string]interface{}{
-					"streams": jsonMap,
-				})
-			} else {
-				cli.Printer.Table(printer.TableData{
-					Header:              []string{"Stream urn", "Stream consistency model", "Sub stream count"},
-					Data:                data,
-					MissingTableDataMsg: printer.NotFoundMsg{Types: "health stream"},
-				})
-			}
+			printStreamList(cli, streamList)
 		}
 		return nil
+	}
+}
+
+func printSubStreamList(cli *di.Deps, subList *stackstate_api.SubStreamList) {
+	data := make([][]interface{}, 0)
+	jsonMap := make([]map[string]interface{}, 0)
+	for _, v := range subList.GetSubStreams() {
+		data = append(data, []interface{}{v.GetSubStreamId(), v.GetCheckStateCount()})
+		jsonMap = append(jsonMap, map[string]interface{}{
+			"sub_stream_id":     v.GetSubStreamId(),
+			"check_state_count": v.GetCheckStateCount(),
+		})
+	}
+
+	if cli.IsJson() {
+		cli.Printer.PrintJson(map[string]interface{}{
+			"sub-stream": jsonMap,
+		})
+	} else {
+		cli.Printer.Table(printer.TableData{
+			Header:              []string{"Sub stream id", "Check state count"},
+			Data:                data,
+			MissingTableDataMsg: printer.NotFoundMsg{Types: "health sub-stream"},
+		})
+	}
+}
+
+func printStreamList(cli *di.Deps, streamList *stackstate_api.StreamList) {
+	data := make([][]interface{}, 0)
+	jsonMap := make([]map[string]interface{}, 0)
+	for _, v := range streamList.GetItems() {
+		data = append(data, []interface{}{v.GetUrn(), v.GetConsistencyModel(), v.GetSubStreams()})
+		jsonMap = append(jsonMap, map[string]interface{}{
+			"stream_urn":               v.GetUrn(),
+			"stream_consistency_model": v.GetConsistencyModel(),
+			"sub_stream_count":         v.GetSubStreams(),
+		})
+	}
+	if cli.IsJson() {
+		cli.Printer.PrintJson(map[string]interface{}{
+			"streams": jsonMap,
+		})
+	} else {
+		cli.Printer.Table(printer.TableData{
+			Header:              []string{"Stream urn", "Stream consistency model", "Sub stream count"},
+			Data:                data,
+			MissingTableDataMsg: printer.NotFoundMsg{Types: "health stream"},
+		})
 	}
 }
