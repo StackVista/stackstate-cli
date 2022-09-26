4759e1311e31331ded523ee3326dd1f51274bcbd
Kajetan Rzepecki <k@spartan.works>
2022-09-26 12:04:55 +0200

    Added parameters to the retention command.

--- cmd/graph/retention.go
+++ cmd/graph/retention.go
@@ -2,39 +2,63 @@ package graph
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 	"gitlab.com/stackvista/stackstate-cli2/generated/stackstate_api"
 	"gitlab.com/stackvista/stackstate-cli2/internal/common"
 	"gitlab.com/stackvista/stackstate-cli2/internal/di"
+	"gitlab.com/stackvista/stackstate-cli2/pkg/pflags"
 )
 
+const (
+	Set = "set"
+	SetShort = "s"
+	ScheduleRemoval = "schedule-removal"
+)
+
+type RetentionArgs struct {
+	Set time.Duration
+	ScheduleRemoval bool
+}
+
 func RetentionCommand(deps *di.Deps) *cobra.Command {
+	args := &RetentionArgs{}
 	cmd := &cobra.Command{
 		Use:   "retention",
 		Short: "Manage the StackState Graph data retention.",
 		Long:  "Manage the StackState Graph data retention.",
-		RunE:  deps.CmdRunEWithApi(runRetentionCommand),
+		RunE:  deps.CmdRunEWithApi(RunRetentionCommand(args)),
 	}
+
+	pflags.DurationVarP(cmd.Flags(), &args.Set, Set, SetShort, pflags.Week, "New data retention window.")
+	cmd.Flags().BoolVar(&args.ScheduleRemoval, ScheduleRemoval, false, "Schedule removal of expired data.")
 
 	return cmd
 }
 
-func runRetentionCommand(cmd *cobra.Command, cli *di.Deps, api *stackstate_api.APIClient, serverInfo *stackstate_api.ServerInfo) common.CLIError {
+func RunRetentionCommand(args *RetentionArgs) di.CmdWithApiFn {
+	return func(
+		cmd *cobra.Command,
+		cli *di.Deps,
+		api *stackstate_api.APIClient,
+		serverInfo *stackstate_api.ServerInfo,
+	) common.CLIError {
 
-	// TODO
-	window := 691200000
-	epoch := 1663493504140000000
+		// TODO
+		window := 691200000
+		epoch := 1663493504140000000
 
-	if cli.IsJson() {
-		cli.Printer.PrintJson(map[string]interface{}{
-			"window": window,
-			"epoch": epoch,
-		})
-	} else {
-		cli.Printer.Success(fmt.Sprintf("Retention window: %d milliseconds", window))
-		cli.Printer.Success(fmt.Sprintf("Epoch transactionId: %d", epoch))
+		if cli.IsJson() {
+			cli.Printer.PrintJson(map[string]interface{}{
+				"window": window,
+				"epoch": epoch,
+			})
+		} else {
+			cli.Printer.Success(fmt.Sprintf("Retention window: %d milliseconds", window))
+			cli.Printer.Success(fmt.Sprintf("Epoch transactionId: %d", epoch))
+		}
+
+		return nil
 	}
-
-	return nil
 }
