08a6b9a4695cf3766a849a592ec64c55aa56430f
Kajetan Rzepecki <k@spartan.works>
2022-10-24 10:42:07 +0200

    Add topology sync list tests.

---
+++ cmd/topology_sync/topology_sync_list_test.go
@@ -0,0 +1,62 @@
+package topology_sync
+
+import (
+	"testing"
+
+	"github.com/stackvista/stackstate-cli/generated/stackstate_api"
+	"github.com/stackvista/stackstate-cli/internal/di"
+	"github.com/stackvista/stackstate-cli/internal/printer"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	Topo1 = stackstate_api.NewTopologyStreamListItem(int64(23), "Topo 1", 0, 0, 0, 0, 0, stackstate_api.TOPOLOGYSYNCSTATUS_RUNNING)
+	Topo2 = stackstate_api.NewTopologyStreamListItem(5, "Topo 2", 23, 5, 23, 5, 0, stackstate_api.TOPOLOGYSYNCSTATUS_DELETING)
+
+	Topo3 = stackstate_api.NewTopologyStreamListItem(13, "Topo 3", 12345, 23, 23, 12345, 1, stackstate_api.TOPOLOGYSYNCSTATUS_DELETE_FAILED)
+
+	Topo4 = stackstate_api.NewTopologyStreamListItem(7, "Topo 4", 12345, 12345, 12345, 12345, 23, stackstate_api.TOPOLOGYSYNCSTATUS_RESETTING)
+
+	AllTopos = []stackstate_api.TopologyStreamListItem{*Topo1, *Topo2, *Topo3, *Topo4}
+
+	SomeTopos = stackstate_api.NewTopologyStreamList(AllTopos)
+)
+
+func TestTopologySyncListTable(t *testing.T) {
+	cli := di.NewMockDeps(t)
+	cmd := ListCommand(&cli.Deps)
+	cli.MockClient.ApiMocks.TopologySynchronizationApi.GetTopologySynchronizationStreamsResponse.Result = *SomeTopos
+
+	di.ExecuteCommandWithContextUnsafe(&cli.Deps, cmd)
+
+	expectedTableCall := []printer.TableData{
+		{
+			Header: []string{"Id", "Name", "Identifier", "Status", "Components", "Relations", "Errors"},
+			Data: [][]interface{}{
+				{Topo1.NodeId, Topo1.Name, "-", Topo1.Status, "+0       -0", "+0       -0", int64(0)},
+				{Topo2.NodeId, Topo2.Name, "-", Topo2.Status, "+23      -5", "+23      -5", int64(0)},
+				{Topo3.NodeId, Topo3.Name, "-", Topo3.Status, "+23   -12345", "+12345   -23", int64(1)},
+				{Topo4.NodeId, Topo4.Name, "-", Topo4.Status, "+12345 -12345", "+12345 -12345", int64(23)},
+			},
+			MissingTableDataMsg: printer.NotFoundMsg{Types: "synchronizations"},
+		},
+	}
+
+	assert.Equal(t, expectedTableCall, *cli.MockPrinter.TableCalls)
+}
+
+func TestTopologySyncListJson(t *testing.T) {
+	cli := di.NewMockDeps(t)
+	cmd := ListCommand(&cli.Deps)
+	cli.MockClient.ApiMocks.TopologySynchronizationApi.GetTopologySynchronizationStreamsResponse.Result = *SomeTopos
+
+	di.ExecuteCommandWithContextUnsafe(&cli.Deps, cmd, "-o", "json")
+
+	expected := []map[string]interface{}{
+		{
+			"synchronizations": AllTopos,
+		},
+	}
+
+	assert.Equal(t, expected, *cli.MockPrinter.PrintJsonCalls)
+}
