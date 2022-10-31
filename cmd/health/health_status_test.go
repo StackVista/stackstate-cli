7e4ca3ef94f8bec359fd3c98b81fb522700b8cb6
Kajetan Rzepecki <k@spartan.works>
2022-10-31 10:09:51 +0100

    Refactored health state tests.

--- cmd/health/health_status_test.go
+++ cmd/health/health_status_test.go
@@ -12,11 +12,99 @@ import (
 
 var (
 	recoveryMessage = "status recovery message"
+
+	noVal  = stackstate_api.NewMetricBucketValue()
+	hasVal = (func() *stackstate_api.MetricBucketValue {
+		v := stackstate_api.NewMetricBucketValue()
+		v.SetValue(float64(23.5))
+		return v
+	})()
+
+	SomeMetrics = stackstate_api.HealthStreamMetrics{
+		BucketSizeSeconds: 10,
+		LatencySeconds:    []stackstate_api.MetricBucketValue{*hasVal, *hasVal, *hasVal},
+		MessagePerSecond:  []stackstate_api.MetricBucketValue{*hasVal, *hasVal, *noVal},
+		CreatesPerSecond:  []stackstate_api.MetricBucketValue{*hasVal, *noVal, *noVal},
+		UpdatesPerSecond:  []stackstate_api.MetricBucketValue{*noVal, *noVal, *noVal},
+		// NOTE Last one is an empty array.
+	}
+	SomeErrors = []stackstate_api.HealthStreamError{
+		{
+			Error: ":shrug:",
+		},
+		{
+			Error: "health stream error",
+		},
+	}
+
+	ExpectedErrorsJson = []map[string]interface{}{
+		{
+			"count":   int32(0),
+			"message": ":shrug:",
+		},
+		{
+			"count":   int32(0),
+			"message": "health stream error",
+		},
+	}
+	ExpectedErrorsTable = printer.TableData{
+		Header: []string{"Message", "Count"},
+		Data: [][]interface{}{
+			{":shrug:", int32(0)},
+			{"health stream error", int32(0)},
+		},
+		MissingTableDataMsg: printer.NotFoundMsg{Types: "errors"},
+	}
+
+	ExpectedMetricsJson = []map[string]interface{}{
+		{
+			"name":   "latency seconds",
+			"now-10": float64(23.5),
+			"10-20":  float64(23.5),
+			"20-30":  float64(23.5),
+		},
+		{
+			"name":   "messages per seconds",
+			"now-10": float64(23.5),
+			"10-20":  float64(23.5),
+			"20-30":  "-",
+		},
+		{
+			"name":   "creates per seconds",
+			"now-10": float64(23.5),
+			"10-20":  "-",
+			"20-30":  "-",
+		},
+		{
+			"name":   "updates per seconds",
+			"now-10": "-",
+			"10-20":  "-",
+			"20-30":  "-",
+		},
+		{
+			"name":   "deletes per seconds",
+			"now-10": "-",
+			"10-20":  "-",
+			"20-30":  "-",
+		},
+	}
+	ExpectedMetricsTable = printer.TableData{
+		Header: []string{"Metric", "10s ago", "10-20s ago", "20-30s ago"},
+		Data: [][]interface{}{
+			{"latency seconds", float64(23.5), float64(23.5), float64(23.5)},
+			{"messages per seconds", float64(23.5), float64(23.5), "-"},
+			{"creates per seconds", float64(23.5), "-", "-"},
+			{"updates per seconds", "-", "-", "-"},
+			{"deletes per seconds", "-", "-", "-"},
+		},
+		MissingTableDataMsg: printer.NotFoundMsg{Types: "metrics"},
+	}
 )
 
 func setupHealthStatusCmd(t *testing.T) (*di.MockDeps, *cobra.Command) {
 	cli := di.NewMockDeps(t)
 	cmd := HealthStatusCommand(&cli.Deps)
+
 	cli.MockClient.ApiMocks.HealthSynchronizationApi.GetHealthSynchronizationStreamTopologyMatchesResponse.Result = stackstate_api.TopologyMatchResult{
 		MultipleMatchesCheckStates: []stackstate_api.MultipleMatchesCheckState{{
 			CheckStateId:              "1",
@@ -33,11 +121,15 @@ func setupHealthStatusCmd(t *testing.T) (*di.MockDeps, *cobra.Command) {
 				ExpiryIntervalMs: 2000,
 			},
 		},
+		Errors:  SomeErrors,
+		Metrics: SomeMetrics,
 	}
 
 	cli.MockClient.ApiMocks.HealthSynchronizationApi.GetHealthSynchronizationStreamStatusResponse.Result = stackstate_api.HealthStreamStatus{
 		RecoverMessage:   &recoveryMessage,
 		ConsistencyModel: "stream consistency model",
+		GlobalErrors:     SomeErrors,
+		AggregateMetrics: SomeMetrics,
 		MainStreamStatus: &stackstate_api.HealthSubStreamStatus{
 			CheckStateCount: 2,
 			SubStreamState: stackstate_api.HealthSubStreamConsistencyState{
@@ -46,12 +138,8 @@ func setupHealthStatusCmd(t *testing.T) (*di.MockDeps, *cobra.Command) {
 					ExpiryIntervalMs: 2000,
 				},
 			},
-			Errors: []stackstate_api.HealthStreamError{{
-				Error: "health stream error",
-			}},
-			Metrics: stackstate_api.HealthStreamMetrics{
-				BucketSizeSeconds: 10,
-			},
+			Errors:  SomeErrors,
+			Metrics: SomeMetrics,
 		},
 	}
 
@@ -96,26 +184,13 @@ func TestHealthStatusWithSubStreamPrintToText(t *testing.T) {
 	expectedTableCalls := []printer.TableData{
 		{
 			Header: []string{"Repeat Interval", "Expiry Interval", "Snapshot Repeat Interval", "Snapshot Expiry Interval", "Checkpoint Offset", "Checkpoint Batch Index"},
-			Data: [][]interface {}{
+			Data: [][]interface{}{
 				{"2000 ms", "2000 ms", "-", "-", "-", "-"},
 			},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"errors"},
+			MissingTableDataMsg: printer.NotFoundMsg{Types: "errors"},
 		},
-		{
-			Header: []string{"Metric", "0s ago", "0-0s ago", "0-0s ago"},
-			Data: [][]interface {}{
-				{"latency seconds", "-", "-", "-"},
-				{"messages per seconds", "-", "-", "-"},
-				{"creates per seconds", "-", "-", "-"},
-				{"updates per seconds", "-", "-", "-"},
-				{"deletes per seconds", "-", "-", "-"},
-			},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"metrics"},
-		},
-		{
-			Header: []string{"Message", "Count"},
-			Data: [][]interface {}{},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"errors"}},
+		ExpectedMetricsTable,
+		ExpectedErrorsTable,
 	}
 	assert.Equal(t, expectedTableCalls, *cli.MockPrinter.TableCalls)
 	assert.True(t, cli.MockPrinter.HasNonJsonCalls)
@@ -124,41 +199,15 @@ func TestHealthStatusWithSubStreamPrintToText(t *testing.T) {
 func TestHealthStatusWithSubStreamPrintToJson(t *testing.T) {
 	cli, cmd := setupHealthStatusCmd(t)
 	di.ExecuteCommandWithContextUnsafe(&cli.Deps, cmd, "status", "--urn", "test:urn", "--sub-stream-urn", "dummy-sub-stream", "-o", "json")
-	assert.Equal(t, []map[string]interface {}{
+	assert.Equal(t, []map[string]interface{}{
 		{
 			"check-state-count": int32(12),
-			"consistency-state": map[string]interface {}{
+			"consistency-state": map[string]interface{}{
 				"expiry-interval": int32(2000),
 				"repeat-interval": int32(2000),
 			},
-			"errors": []map[string]interface {}{},
-			"metrics": []map[string]interface {}{
-				{
-					"name": "latency seconds",
-					"now-0": "-",
-					"0-0": "-",
-				},
-				{
-					"name": "messages per seconds",
-					"now-0": "-",
-					"0-0": "-",
-				},
-				{
-					"name": "creates per seconds",
-					"now-0": "-",
-					"0-0": "-",
-				},
-				{
-					"name": "updates per seconds",
-					"now-0": "-",
-					"0-0": "-",
-				},
-				{
-					"name": "deletes per seconds",
-					"now-0": "-",
-					"0-0": "-",
-				},
-			},
+			"errors":  ExpectedErrorsJson,
+			"metrics": ExpectedMetricsJson,
 		},
 	}, *cli.MockPrinter.PrintJsonCalls)
 }
@@ -178,47 +227,17 @@ func TestHealthStatusStreamPrintToText(t *testing.T) {
 	}
 	assert.Equal(t, expectedSuccessCalls, *cli.MockPrinter.PrintLnCalls)
 	expectedTableCalls := []printer.TableData{
-		{
-			Header: []string{"Metric", "0s ago", "0-0s ago", "0-0s ago"},
-			Data: [][]interface {}{
-				{"latency seconds", "-", "-", "-"},
-				{"messages per seconds", "-", "-", "-"},
-				{"creates per seconds", "-", "-", "-"},
-				{"updates per seconds", "-", "-", "-"},
-				{"deletes per seconds", "-", "-", "-"},
-			},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"metrics"},
-		},
-		{
-			Header: []string{"Message", "Count"},
-			Data: [][]interface {}{},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"errors"},
-		},
+		ExpectedMetricsTable,
+		ExpectedErrorsTable,
 		{
 			Header: []string{"Repeat Interval", "Expiry Interval", "Snapshot Repeat Interval", "Snapshot Expiry Interval", "Checkpoint Offset", "Checkpoint Batch Index"},
-			Data: [][]interface {}{
+			Data: [][]interface{}{
 				{"2000 ms", "2000 ms", "-", "-", "-", "-"},
 			},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"errors"},
+			MissingTableDataMsg: printer.NotFoundMsg{Types: "errors"},
 		},
-		{
-			Header: []string{"Metric", "10s ago", "10-20s ago", "20-30s ago"},
-			Data: [][]interface {}{
-				{"latency seconds", "-", "-", "-"},
-				{"messages per seconds", "-", "-", "-"},
-				{"creates per seconds", "-", "-", "-"},
-				{"updates per seconds", "-", "-", "-"},
-				{"deletes per seconds", "-", "-", "-"},
-			},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"metrics"},
-		},
-		{
-			Header: []string{"Message", "Count"},
-			Data: [][]interface {}{
-				{"health stream error", int32(0)},
-			},
-			MissingTableDataMsg: printer.NotFoundMsg{Types:"errors"},
-		},
+		ExpectedMetricsTable,
+		ExpectedErrorsTable,
 	}
 	assert.Equal(t, expectedTableCalls, *cli.MockPrinter.TableCalls)
 	assert.True(t, cli.MockPrinter.HasNonJsonCalls)
@@ -227,82 +246,20 @@ func TestHealthStatusStreamPrintToText(t *testing.T) {
 func TestHealthStatusStreamPrintToJson(t *testing.T) {
 	cli, cmd := setupHealthStatusCmd(t)
 	di.ExecuteCommandWithContextUnsafe(&cli.Deps, cmd, "status", "--urn", "test:urn", "-o", "json")
-	assert.Equal(t, []map[string]interface {}{
+	assert.Equal(t, []map[string]interface{}{
 		{
 			"consistency-model": "stream consistency model",
-			"errors": []map[string]interface {}{},
-			"main-stream": map[string]interface {}{
+			"main-stream": map[string]interface{}{
 				"check-state-count": int32(2),
-				"consistency-state": map[string]interface {}{
+				"consistency-state": map[string]interface{}{
 					"expiry-interval": int32(2000),
 					"repeat-interval": int32(2000),
 				},
-				"errors": []map[string]interface {}{
-					{
-						"count": int32(0),
-						"message": "health stream error",
-					},
-				},
-				"metrics":[]map[string]interface {}{
-					{
-						"name":"latency seconds",
-						"now-10":"-",
-						"10-20":"-",
-						"20-30":"-",
-					},
-					{
-						"name":"messages per seconds",
-						"now-10":"-",
-						"10-20":"-",
-						"20-30":"-",
-					},
-					{
-						"name":"creates per seconds",
-						"now-10":"-",
-						"10-20":"-",
-						"20-30":"-",
-					},
-					{
-						"name":"updates per seconds",
-						"now-10":"-",
-						"10-20":"-",
-						"20-30":"-",
-					},
-					{
-						"name":"deletes per seconds",
-						"now-10":"-",
-						"10-20":"-",
-						"20-30":"-",
-					},
-				},
+				"errors":  ExpectedErrorsJson,
+				"metrics": ExpectedMetricsJson,
 			},
-			"metrics": []map[string]interface {}{
-				{
-					"name":"latency seconds",
-					"now-0":"-",
-					"0-0":"-",
-				},
-				{
-					"name":"messages per seconds",
-					"now-0":"-",
-					"0-0":"-",
-				},
-				{
-					"name":"creates per seconds",
-					"now-0":"-",
-					"0-0":"-",
-				},
-				{
-					"name":"updates per seconds",
-					"now-0":"-",
-					"0-0":"-",
-				},
-				{
-					"name":"deletes per seconds",
-					"now-0":"-",
-					"0-0":"-",
-				},
-			},
+			"errors":  ExpectedErrorsJson,
+			"metrics": ExpectedMetricsJson,
 		},
 	}, *cli.MockPrinter.PrintJsonCalls)
 }
