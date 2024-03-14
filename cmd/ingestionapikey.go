47d8615d546f006217cf49fdc1970f78af545058
Lukasz Marchewka <lukasz.marchewka@stackstate.com>
2024-03-14 15:50:42 +0100

    STAC-20950 command to create ingestion keys

---
+++ cmd/ingestionapikey.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/stackvista/stackstate-cli/cmd/ingestionapikey"
+	"github.com/stackvista/stackstate-cli/internal/di"
+)
+
+func IngestionApiKeyCommand(deps *di.Deps) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "ingestion-api-key",
+		Short: "Manage Ingestion API Keys",
+		Long:  "Manage API Keys used by ingestion pipelines, means data (spans, metrics, logs an so on) send by STS Agent, OTel and so on.",
+	}
+
+	cmd.AddCommand(ingestionapikey.CreateCommand(deps))
+	return cmd
+}
---
+++ cmd/ingestionapikey/ingestionapikey_create.go
@@ -0,0 +1,71 @@
+package ingestionapikey
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gookit/color"
+	"github.com/spf13/cobra"
+	"github.com/stackvista/stackstate-cli/generated/stackstate_api"
+	"github.com/stackvista/stackstate-cli/internal/common"
+	"github.com/stackvista/stackstate-cli/internal/di"
+)
+
+const (
+	DateFormat = "2006-01-02"
+)
+
+type CreateArgs struct {
+	Name        string
+	Expiration  time.Time
+	Description string
+}
+
+func CreateCommand(deps *di.Deps) *cobra.Command {
+	args := &CreateArgs{}
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a new Ingestion Api Key",
+		Long:  "Creates a token and then returns it in the response, the token can't be obtained any more after that so store it in the safe space",
+		RunE:  deps.CmdRunEWithApi(RunIngestionApiKeyGenerationCommand(args)),
+	}
+
+	common.AddRequiredNameFlagVar(cmd, &args.Name, "Name of the API Key")
+	cmd.Flags().TimeVar(&args.Expiration, "expiration", time.Time{}, []string{DateFormat}, "Expiration date of the API Key")
+	cmd.Flags().StringVar(&args.Description, "description", "", "Optional description of the API Key")
+	return cmd
+}
+
+func RunIngestionApiKeyGenerationCommand(args *CreateArgs) di.CmdWithApiFn {
+	return func(cmd *cobra.Command, cli *di.Deps, api *stackstate_api.APIClient, serverInfo *stackstate_api.ServerInfo) common.CLIError {
+		req := stackstate_api.GenerateIngestionApiKeyRequest{
+			Name: args.Name,
+		}
+
+		if len(args.Description) > 0 {
+			req.Description = &args.Description
+		}
+
+		if !args.Expiration.IsZero() {
+			m := args.Expiration.UnixMilli()
+			req.Expiration = &m
+		}
+
+		ingestionApiKeyAPI := api.IngestionApiKeyApi.GenerateIngestionApiKey(cli.Context)
+
+		serviceToken, resp, err := ingestionApiKeyAPI.GenerateIngestionApiKeyRequest(req).Execute()
+		if err != nil {
+			return common.NewResponseError(err, resp)
+		}
+
+		if cli.IsJson() {
+			cli.Printer.PrintJson(map[string]interface{}{
+				"ingestion-api-key": serviceToken,
+			})
+		} else {
+			cli.Printer.Success(fmt.Sprintf("Ingestion API Key generated: %s\n", color.White.Render(serviceToken.ApiKey)))
+		}
+
+		return nil
+	}
+}
