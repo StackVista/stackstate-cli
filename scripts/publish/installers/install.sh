b6b0afe7d6619b5f40b7826e8ddb74b49a9eb464
Jeroen van Erp <jvanerp@stackstate.com>
2024-05-16 09:45:36 +0200

    STAC-21285: Fix OS detection for CLI installation script
    Signed-off-by: Jeroen van Erp <jvanerp@stackstate.com>

--- scripts/publish/installers/install.sh
+++ scripts/publish/installers/install.sh
@@ -21,6 +21,8 @@ function error() {
 
 if [[ "$OSTYPE" == "linux"* ]]; then
   OS=linux
+elif [[ "$OSTYPE" == "linux" ]]; then
+  OS=linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   OS=darwin
 else
