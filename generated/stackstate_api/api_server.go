38b5535e4ad8f42084d15106c8a154c7266fa2bf
Alejandro Acevedo <alejandro.acevedo@suse.com>
2025-07-16 16:56:22 +0200

    STAC-23051: Make error when trying out connection to platform more informative

--- generated/stackstate_api/api_server.go
+++ generated/stackstate_api/api_server.go
@@ -165,6 +165,16 @@ func (a *ServerApiService) ServerInfoExecute(r ApiServerInfoRequest) (*ServerInf
 			body:  localVarBody,
 			error: localVarHTTPResponse.Status,
 		}
+		if localVarHTTPResponse.StatusCode == 401 {
+			var v GenericErrorsResponse
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
 		if localVarHTTPResponse.StatusCode == 500 {
 			var v GenericErrorsResponse
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
